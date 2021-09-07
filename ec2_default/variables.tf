714a382cb1b5ff0ea43384e7a206e1b3cedfc904
Nathalia Sanada <sanadanathalia@gmail.com>
2021-09-07 19:54:13 -0300

    fix variables

--- ec2_default/variables.tf
+++ ec2_default/variables.tf
@@ -25,9 +25,9 @@ variable vpc_cidr_block {
   default     = "10.0.0.0/20"
 }
 
-variable priv_cidr_block {
+variable pub_cidr_block {
   type        = string
-  default     = "10.0.1.0/25"
+  default     = "10.0.2.0/25"
 }
 
 variable network_name {
